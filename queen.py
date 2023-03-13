5e4306d99e8f7962352cc43fa34ba375e2b25a11
pandeyganesh <pandeyganesha@gmail.com>
2023-03-13 18:44:06 +0530

    completed selection and cross over, but still need to improve cross over method.

--- queen.py
+++ queen.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 
 class nqueen:
@@ -5,12 +6,14 @@ class nqueen:
     def __init__(self, n) -> None:
         self.n = n
         self.population = None
+        self.times=2
 
-    def populate(self, times):
+    def populate(self):
         """
-        It creates a poplution of random places (chromosome) for queens in each column"""
+        It creates a poplution of random places (chromosome) for queens in each column.
+        We will be taking even number for population for easy crossing over. (Won't create any differnce in working of This algo.)"""
 
-        self.population = np.random.randint(0, self.n, size=(times, self.n))
+        self.population = np.random.randint(0, self.n, size=(self.times, self.n))
 
     def fitness(self):
         """
@@ -20,21 +23,26 @@ class nqueen:
         mnm = 0
         fit = []
         fit_perecent=[]
-        for ind1, pop in enumerate(self.population):
+        for ind1, pop in enumerate(self.population): #iterate over each chromosome
             pair=0
+
+            #nested for loop to iterate over each value of chromosome and do comparisons with each pair possible.
             for i in range(len(pop)):
                 for j in range(i+1, len(pop)):
+
+                    # If a pair of queen is not attacking each other then + 1
                     if not self.check_attack((pop[i], i), (pop[j], j)):
                         pair+=1
             fit.append(pair)
 
         total = sum(fit)
         
-        for i in range(self.population.shape[0]):
-            fit_perecent.append(round(fit[i]/total, 2))
+        for i in range(self.times):
+            fit_perecent.append(round(fit[i]*100/total, 2))
         
         print("fit percent", fit_perecent)
         print("fit", fit)
+        return fit_perecent
                     
                 
     def check_attack(self, cord1, cord2):
@@ -42,22 +50,54 @@ class nqueen:
         returns True if pair are attacking each other else False
         """
 
+        # if queens in same row
         if cord1[0]==cord2[0]:
             return True
 
+        #if queens in same diagonal
         if abs(cord1[0]-cord2[0])==abs(cord1[1]-cord2[1]):
             return True
         
         return False
 
-    def selection():
+    def selection(self):
         """
         For producing the next generation, it will create parents on the basis of fitness and probability and will also select the crossing over point.
         """
+        selected = random.choices(np.arange(0, self.times), self.fitness(), k=self.times)
+        d={}
+        for i in selected:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        print(d)
+        print("selected", selected)
+        return selected
 
-    def crossover():
+
+    def crossover(self):
         """
-        The actual process of crossing over will be performed here."""
+        The actual process of crossing over will be performed here.
+        """
+        new_pop = np.empty((self.times, self.n), dtype=int)
+        sel = self.selection()
+        for i in range(0, len(sel), 2):
+            point = random.randint(1, self.n-1)
+            for j in range(0, point):
+                new_pop[i][j] = self.population[sel[i]][j]
+                new_pop[i+1][j] = self.population[sel[i+1]][j]
+            for j in range(point, self.n):
+                new_pop[i][j] = self.population[sel[i+1]][j]
+                new_pop[i+1][j] = self.population[sel[i]][j]
+
+        self.population = new_pop
+        for pop in self.population:
+            for j in pop:
+                print(j, end=" ")
+            print("")
+            
+
 
     def mutation():
         """"
@@ -65,6 +105,6 @@ class nqueen:
         """
 
 q = nqueen(4)
-q.populate(6)
+q.populate()
 print(q.population)
-q.fitness()
+q.crossover()
