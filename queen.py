1e86dcbd304d09c5d992a3d7fb18e998edc97c02
pandeyganesh <pandeyganesha@gmail.com>
2023-03-13 11:40:50 +0530

    made a working fitness function to calculate fitness of chromosomes with their percent

--- queen.py
+++ queen.py
@@ -10,14 +10,45 @@ class nqueen:
         """
         It creates a poplution of random places (chromosome) for queens in each column"""
 
-        self.population = np.random.randint(1, self.n+1, size=(times, self.n))
+        self.population = np.random.randint(0, self.n, size=(times, self.n))
 
     def fitness(self):
         """
         It will calculate the fitness in percentage for each chromosome.
         """
-        mxm = ((self.n-1)*self.n)//2
+        mxm = ((self.n-1)*self.n)//2 # maximum number of non attacking pairs possible
         mnm = 0
+        fit = []
+        fit_perecent=[]
+        for ind1, pop in enumerate(self.population):
+            pair=0
+            for i in range(len(pop)):
+                for j in range(i+1, len(pop)):
+                    if not self.check_attack((pop[i], i), (pop[j], j)):
+                        pair+=1
+            fit.append(pair)
+
+        total = sum(fit)
+        
+        for i in range(self.population.shape[0]):
+            fit_perecent.append(round(fit[i]/total, 2))
+        
+        print("fit percent", fit_perecent)
+        print("fit", fit)
+                    
+                
+    def check_attack(self, cord1, cord2):
+        """
+        returns True if pair are attacking each other else False
+        """
+
+        if cord1[0]==cord2[0]:
+            return True
+
+        if abs(cord1[0]-cord2[0])==abs(cord1[1]-cord2[1]):
+            return True
+        
+        return False
 
     def selection():
         """
@@ -33,6 +64,7 @@ class nqueen:
         Some random mutation would be applied to the populations anologous to the evolution.
         """
 
-q = nqueen(8)
-q.populate(5)
+q = nqueen(4)
+q.populate(6)
 print(q.population)
+q.fitness()
